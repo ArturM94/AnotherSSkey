485bef76d1e3361a439c022c70bcaa24b42e236e
Artur Manukian <manukian.artur.94@gmail.com>
2019-04-26 16:26:22 +0300

    Create UserAdmin for User model.

--- keystorage/admin.py
+++ keystorage/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from keystorage.models import User
+
+
+class UserAdmin(admin.ModelAdmin):
+    """User model for admin panel"""
+    list_display = ('id', 'username', 'email', 'first_name', 'last_name', 'phone_number', 'date_joined', 'last_login')
+
+
+admin.site.register(User, UserAdmin)

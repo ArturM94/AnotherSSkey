fa5f416eeefd97fe2b791ca999c8575f3e71759e
Artur Manukian <manukian.artur.94@gmail.com>
2019-04-26 15:45:22 +0300

    Create AbstractPhone model.

--- keystorage/models.py
+++ keystorage/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 from django.db import models
 
-# Create your models here.
+
+class AbstractPhone(models.Model):
+    """Abstract phone number model
+
+    Phone number in accordance with E.164 recommendation.
+    """
+    phone_regex = RegexValidator(
+        regex=r'^\+\d{8,15}$',
+        message="Phone number must be entered in the format: '+99999999'. Up to 15 digits allowed."
+    )
+    phone_number = models.CharField(validators=[phone_regex], verbose_name='phone number', max_length=16, blank=True)
+
+    class Meta:
+        abstract = True
+

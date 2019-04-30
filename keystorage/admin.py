bffec3d09fad7e9a9f980e4a48b46c714bb06a2c
Artur Manukian <manukian.artur.94@gmail.com>
2019-05-01 00:48:01 +0300

    Add UserCreationForm and UserChangeForm for UserAdmin model. Expand UserAdmin model.

--- keystorage/admin.py
+++ keystorage/admin.py
@@ -1,12 +1,72 @@
+from django import forms
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
 from django.contrib.auth.models import Group
 
 from keystorage.models import User
 
 
-class UserAdmin(admin.ModelAdmin):
-    """User model for admin panel"""
-    list_display = ('id', 'username', 'email', 'first_name', 'last_name', 'phone_number', 'date_joined', 'last_login')
+class UserCreationForm(forms.ModelForm):
+    """User creation form
+
+    Form for creating new users
+    """
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email')
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+
+class UserChangeForm(forms.ModelForm):
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password', 'first_name', 'last_name', 'is_active', 'is_staff', 'phone_number')
+
+    def clean_password(self):
+        return self.initial['password']
+
+
+class UserAdmin(BaseUserAdmin):
+    """User admin
+
+    User model for admin panel"""
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    list_display = ('username', 'email', 'id', 'is_staff', 'first_name', 'last_name', 'phone_number', 'date_joined',
+                    'last_login')
+    list_filter = ('is_staff',)
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'phone_number',)}),
+        ('Permissions', {'fields': ('is_staff',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2')}
+         ),
+    )
+    search_fields = ('username', 'email', 'first_name', 'last_name',)
 
 
 admin.site.register(User, UserAdmin)
